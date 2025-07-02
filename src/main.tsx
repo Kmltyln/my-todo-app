0ef5a0c16b4a2dacabb2fa5d9193cb44c97a9154
Taylan <k.taylan.acarkan@gmail.com>
2025-07-02 17:21:08 +0300

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App.tsx";
+import "./index.css";
+import TodosContextProvider from "./contexts/TodosContextProvider.tsx";
+import { KindeProvider } from "@kinde-oss/kinde-auth-react";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+ <KindeProvider
+		clientId="dfdc3958ec0447968dc89de106495f81"
+		domain="https://kmltyln.kinde.com"
+		redirectUri="http://localhost:5173"
+		logoutUri="http://localhost:5173"
+	>
+      <TodosContextProvider>
+        <App />
+      </TodosContextProvider>
+    </KindeProvider>
+  </React.StrictMode>
+);
