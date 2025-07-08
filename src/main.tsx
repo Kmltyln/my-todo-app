00061ebe509b316a8dec8b48ef82859298770020
Taylan <k.taylan.acarkan@gmail.com>
2025-07-08 23:04:57 +0300

    save

--- src/main.tsx
+++ src/main.tsx
@@ -10,8 +10,15 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
  <KindeProvider
 		clientId="dfdc3958ec0447968dc89de106495f81"
 		domain="https://kmltyln.kinde.com"
-		redirectUri="http://localhost:5173"
-		logoutUri="http://localhost:5173"
+		redirectUri={
+      process.env.NODE_ENV==="production"
+      ?"https://my-todo-app-livid-eight.vercel.app/"
+      :"http://localhost:5173"
+    }
+		logoutUri={
+      process.env.NODE_ENV==="production"
+      ?"https://kmltyln.kinde.com"
+      :"http://localhost:5173"}
 	>
       <TodosContextProvider>
         <App />
